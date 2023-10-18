2b59e59b645defa1ce10487a0ff7ca04dc42bab8
Marosdee Uma <danya0365@gmail.com>
2023-10-18 14:03:04 +0700

    chore: update lib

--- src/@core/GameUi.tsx
+++ src/@core/GameUi.tsx
@@ -1,21 +1,21 @@
-import { HTML } from 'drei';
-import React from 'react';
-import { AssetLoaderProvider } from './AssetLoader';
-import { GameContext } from './Game';
-import useGame from './useGame';
+import React from "react";
+import { AssetLoaderProvider } from "./AssetLoader";
+import { GameContext } from "./Game";
+import useGame from "./useGame";
+import { Html } from "@react-three/drei";
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export default function GameUi({ children }: Props) {
-    const gameContextValue = useGame(); // forwarded to dom reconciler
+  const gameContextValue = useGame(); // forwarded to dom reconciler
 
-    return (
-        <HTML eps={1} zIndexRange={[0, 0]}>
-            <GameContext.Provider value={gameContextValue}>
-                <AssetLoaderProvider>{children}</AssetLoaderProvider>
-            </GameContext.Provider>
-        </HTML>
-    );
+  return (
+    <Html eps={1} zIndexRange={[0, 0]}>
+      <GameContext.Provider value={gameContextValue}>
+        <AssetLoaderProvider>{children}</AssetLoaderProvider>
+      </GameContext.Provider>
+    </Html>
+  );
 }
--- src/@core/useGameEvent.ts
+++ src/@core/useGameEvent.ts
@@ -1,19 +1,19 @@
-import { DependencyList, useEffect, useRef } from 'react';
-import { PubSubEvent } from './utils/createPubSub';
-import useGame from './useGame';
+import { DependencyList, useEffect, useRef } from "react";
+import { PubSubEvent } from "./utils/createPubSub";
+import useGame from "./useGame";
 
 export default function useGameEvent<T extends PubSubEvent>(
-    eventName: T['name'],
-    callback: (data: T['data']) => void,
-    deps: DependencyList = []
+  eventName: T["name"],
+  callback: (data: T["data"]) => void,
+  deps: DependencyList = []
 ) {
-    const callbackRef = useRef<typeof callback>();
-    const { subscribe } = useGame();
+  const callbackRef = useRef<typeof callback>(null!);
+  const { subscribe } = useGame();
 
-    callbackRef.current = callback;
+  callbackRef.current = callback;
 
-    useEffect(() => {
-        return subscribe(eventName, callbackRef.current);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [subscribe, eventName, ...deps]);
+  useEffect(() => {
+    return subscribe(eventName, callbackRef.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [subscribe, eventName, ...deps]);
 }
